e6ba0fecdae85d24705721e8d678cca8664ad000
Shahnwaz Khan <shahnwazkhan2007@gmail.com>
2023-08-15 21:42:34 +0530

    deploy

--- index.js
+++ index.js
@@ -19,19 +19,6 @@ app.use(fileUpload({
 
 
 // vercel
-// Your code
-if (process.env.NODE_ENV === "production") {
-    const path = require("path");
-    app.use(express.static(path.resolve(__dirname, 'client', 'build')));
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'),function (err) {
-            if(err) {
-                res.status(500).send(err)
-            }
-        });
-    })
-}
-// Your code
 
 app.get('/', (req, res) => {
     res.send("Please route to correct page.Have a Good Day. IP address is: ")
