2ee45b8825afc635789ef36c96066ed8cb30824f
Shahnwaz Khan <shahnwazkhan2007@gmail.com>
2023-08-16 15:02:13 +0530

    deploy test

--- Components/Connection/DB_Connections.js
+++ Components/Connection/DB_Connections.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const connection =  () => {
     // const connect =  mongoose.connect("mongodb+srv://khanshahnwaz:Anonymous786@andctreasure.e5tjaqw.mongodb.net/?retryWrites=true&w=majority")
-    const connect=mongoose.connect("mongodb+srv://khanshahnwaz:Anonymous786@profile.frrsa4k.mongodb.net/?retryWrites=true&w=majority")
+    const connect=mongoose.connect("mongodb+srv://khanshahnwaz:Anonymous786@profile.frrsa4k.mongodb.net/profile?retryWrites=true&w=majority")
     // const connect=mongoose.connect("mongodb://127.0.1:27017/profile_manager")
     if (connect) {
         console.log("Connection successfull.")
