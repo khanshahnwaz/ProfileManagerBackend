2eda220989725fda97843ff039848523332c7122
Shahnwaz Khan <shahnwazkhan2007@gmail.com>
2023-08-16 12:43:58 +0530

    checking deployment

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ connection();
 const express = require('express');
 const cors = require('cors')
 const app = express();
+app.use(cors())
 
 // Middleware to send post request
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
-app.use(cors())
 
 
 app.use(fileUpload({
